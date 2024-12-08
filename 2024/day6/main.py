062a002dacfa7a9fb21904d70c51334f75aa4948
Jure Bajic <jure.bajic94@gmail.com>
2024-12-08 14:24:28 +0100

    Day 6 part 2

--- 2024/day6/main.py
+++ 2024/day6/main.py
@@ -17,26 +17,17 @@ def get_new_direction(direction):
             return ">"
         case ">":
             return"v" 
-        case "<":
-            return"^" 
         case "v":
             return"<" 
+        case "<":
+            return"^" 
         case _:
             raise Exception("Direction not found")
 
 
-
 def first_part():
-    map = []
-    directions = ["^", ">", "<", "v"]
-    with open("input.txt") as file:
-        for line in file:
-            map.append(line.strip())
-    start_position = [(i, j) for i in range(len(map)) for j in range(len(map[i])) if map[i][j] in directions][0]
-    print(start_position)
-
     visited = set()
-    direction = map[start_position[0]][start_position[1]]
+    direction = start_direction
     current = start_position
     while True:
         visited.add(current)
@@ -51,4 +42,45 @@ def first_part():
     print(f"Number of visited positions is {len(visited)}")
 
 
+def is_there_loop(current, direction):
+    visited = set()
+    while True:
+        change = get_change(direction)
+        ahead = (current[0] + change[0], current[1] + change[1])
+        if ahead[0] < 0 or ahead[0] >= len(map) or ahead[1] < 0 or ahead[1] >= len(map[0]):
+            break
+        if map[ahead[0]][ahead[1]] == "#":
+            if (ahead[0], ahead[1], direction) in visited:
+                return True
+            visited.add((ahead[0], ahead[1], direction))
+            direction = get_new_direction(direction)
+        else:
+            current = ahead
+    return False
+
+
+def second_part():
+#     current = start_position
+    # direction = start_direction
+    loop_causing_obstacle_positions = set()
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == ".":
+                old = map[i][j]
+                map[i][j] = "#"
+                if is_there_loop(start_position, start_direction):
+                    loop_causing_obstacle_positions.add((i, j))
+                map[i][j] = old
+
+    print(f"Number of possible new obstacles is {len(loop_causing_obstacle_positions)}")
+
+map = []
+directions = ["^", ">", "<", "v"]
+with open("input.txt") as file:
+    for line in file:
+        map.append(list(line.strip()))
+start_position = [(i, j) for i in range(len(map)) for j in range(len(map[i])) if map[i][j] in directions][0]
+start_direction = map[start_position[0]][start_position[1]]
+
 first_part()
+second_part()
